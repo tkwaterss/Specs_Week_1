2bdd3156af98bf6b98b7b32ed65a7360a3182227
Tobin Waters <tkwaterss@gmail.com>
2023-03-21 08:23:25 -0600

    day 2 start

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const { title, amount, date } = props;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
