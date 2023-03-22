bab80f3b90d226fbe811d7974baf464848db55fa
Tobin Waters <tkwaterss@gmail.com>
2023-03-22 09:10:04 -0600

    updated demo app

--- demoPractice/demo-app/src/App.js
+++ demoPractice/demo-app/src/App.js
@@ -2,12 +2,32 @@ import "./App.css";
 import Greeting from "./components/greeting";
 
 function App() {
-  let greeting = "Get Lost";
-  let name = "Tobin";
+  let myPeople = [
+    {
+      name: "Tobin",
+      age: 29,
+      isCool: false
+    },
+    {
+      name: "Billy",
+      age: 25,
+      isCool: true
+    },
+    {
+      name: "Bobby",
+      age: 19,
+      isCool: true
+    },
+  ]
+ 
+  let peopleDisplay = myPeople.map(person => {
+    return <Greeting personalInfo={person} />
+  })
+
   return (
     <div className="App">
-      <Greeting />
-      <Greeting />
+      <h1>These are my people!</h1>
+      {peopleDisplay}
     </div>
   );
 }
--- demoPractice/demo-app/src/components/greeting.jsx
+++ demoPractice/demo-app/src/components/greeting.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-const Greeting = () => {
+const Greeting = (props) => {
+  const {name, age, isCool} = props.personalInfo
+
+  // if(name !== "Tobin") {
+  //   return(
+  //   <div>
+  //     <h1>You'r not Tobin!</h1>
+  //   </div>)
+  // }
+
   return (
     <div>
-      <h1>Greeting</h1>
+      <h1>Greetings {name}</h1>
+      <h2>Age: {age}</h2>
+      <h3>{isCool ? "This person is dope" : "This person is pretty Lame"}</h3>
     </div>
   );
 };
