867e69a5f2fa469c87004ebd3bdc0ad4c554a64f
Tobin Waters <tkwaterss@gmail.com>
2023-03-27 09:35:51 -0600

    updates

--- review-project/src/App.js
+++ review-project/src/App.js
@@ -6,6 +6,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AccountPage from "./components/AccountPage/AccountPage";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import "./App.css";
 
 function App() {
--- review-project/src/components/Footer.jsx
+++ review-project/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './Footer.css';
+import Button from '@mui/material/Button';
 
 const Footer = () => {
     return (
         <div className="footer" >
             <h2>Footer</h2>
+            <Button variant="contained">Hello World</Button>
         </div>
     )
 }
